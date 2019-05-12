17acb9dbd67e059dadfa671ca1bce09b725809b4
Mochamadwildan <mochamadwildan354@gmail.com>
2019-05-12 21:52:27 +0700

    update file

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment} from '../../environments/environment';
 
 @Component({
   selector: 'navbar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  backgroundColor = environment.navBarBackgroundColor;
 
   ngOnInit() {
   }
